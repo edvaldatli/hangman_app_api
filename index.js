de0daf88d34d4157377a9a2193989364345c12c5
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2023-11-29 20:43:18 +0000

    basic express api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/hangman', (req, res) => {
+    const { language, difficulty } = req.query;
+
+    res.json({
+        message: 'Hangman Game API',
+        language: language || 'not specified',
+        difficulty: difficulty || 'not specified'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
