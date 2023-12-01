f143ea7c5c9011958045d23708c9c75bf969dfb0
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2023-12-01 10:23:57 +0000

    update

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.get('/hangman', (req, res) => {
-    const { language, difficulty } = req.query;
+app.get('/hangman/:language/:difficulty', (req, res) => {
+    const { language, difficulty } = req.params;
 
-    res.json({
-        message: 'Hangman Game API',
-        language: language || 'not specified',
-        difficulty: difficulty || 'not specified'
+    let wordsArray;
+    let requestedWord;
+
+    fs.readFile('db/words.json', 'utf8', (err, data) => {
+        if (err){
+            console.error('Error reading file:', e);
+            return
+        }
+
+        try {
+            wordsArray = JSON.parse(data)
+        } catch (e) {
+            console.error('Error parsing JSON:', e);
+            return;
+        }
+
+        requestedWord = wordsArray.find(wordObj => wordObj.word === 'orange');
+
+        res.json({
+            message: 'Hangman Game API',
+            language: language || 'not specified',
+            difficulty: difficulty || 'not specified',
+            word: requestedWord.word
+        });
     });
+    
+    console.log('A user gathered a word');
 });
 
 app.listen(port, () => {
